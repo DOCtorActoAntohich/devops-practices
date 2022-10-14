27d8a1a9b2773d209be43d0c303edfd43120a12d
DOCtorActoAntohich <fairy666death@mail.ru>
2022-10-14 13:27:55 +0300

    Add metrics to python app

--- app_python/main.py
+++ app_python/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from starlette.responses import JSONResponse
 from starlette.requests import Request as StarletteRequest
 from starlette.status import HTTP_400_BAD_REQUEST
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from app_python.errors import InvalidTimeZoneError
 from app_python.interactor import TimeInteractor
@@ -15,11 +16,16 @@ async def on_invalid_id_error(_request: StarletteRequest, exc: InvalidTimeZoneEr
     return JSONResponse({"error_details": f"{exc}"}, status_code=HTTP_400_BAD_REQUEST)
 
 
+@app.on_event("startup")
+async def startup():
+    Instrumentator().instrument(app).expose(app)
+
+
 @app.get("/")
 async def read_root() -> str:
     return TimeInteractor.get_time_in("Europe/Moscow")
 
 
-@app.get("/{iana_time_zone:path}")
+@app.get("/zone/{iana_time_zone:path}")
 async def read_by_timezone(iana_time_zone: str) -> str:
     return TimeInteractor.get_time_in(iana_time_zone)
